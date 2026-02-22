bb9db828515d51b3e4073e42421d8d5babc3bd6e
anaclaramartinelli20 <anaclara@AnaClara.localdomain>
2026-02-22 15:05:10 -0300

    ex 3

--- exe3/main.c
+++ exe3/main.c
@@ -1,15 +1,13 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include <stdio.h>
-#include <stdbool.h>
 
 const int BTN_VERDE = 26;
 const int BTN_VERMELHO = 28;
-
 const int LED_VERDE = 4;
 const int LED_VERMELHO = 6;
 
-int main(void) {
+int main() {
   stdio_init_all();
 
   gpio_init(BTN_VERDE);
@@ -22,23 +20,35 @@ int main(void) {
 
   gpio_init(LED_VERDE);
   gpio_set_dir(LED_VERDE, GPIO_OUT);
+  gpio_put(LED_VERDE, 0);
 
   gpio_init(LED_VERMELHO);
   gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+  gpio_put(LED_VERMELHO, 0);
+
+  int estado_led_verde = 0;
+  int estado_led_vermelho = 0;
 
   while (true) {
     if (!gpio_get(BTN_VERDE)) {
-      gpio_put(LED_VERDE, 1);
-      while (!gpio_get(BTN_VERDE)) tight_loop_contents();
-        gpio_put(LED_VERDE, 0);
+      estado_led_verde = !estado_led_verde;
+      gpio_put(LED_VERDE, estado_led_verde);
+            
+      while (!gpio_get(BTN_VERDE)) {
+        tight_loop_contents();
+      }
+
     }
 
     if (!gpio_get(BTN_VERMELHO)) {
-      gpio_put(LED_VERMELHO, 1);
-      while (!gpio_get(BTN_VERMELHO)) tight_loop_contents();
-        gpio_put(LED_VERMELHO, 0);
+      estado_led_vermelho = !estado_led_vermelho;
+      gpio_put(LED_VERMELHO, estado_led_vermelho);
+      
+      while (!gpio_get(BTN_VERMELHO)) {
+          tight_loop_contents();
+      }
     }
 
-    tight_loop_contents();
-  }  
-} 
+  tight_loop_contents();
+  }
+}
