0edb78b9ae7ed244762fb1af9cf518e0c4dd8c28
anaclaramartinelli20 <anaclara@AnaClara.localdomain>
2026-02-22 18:18:21 -0300

    ex5

--- exe5/main.c
+++ exe5/main.c
@@ -8,24 +8,44 @@ const int BTN_PIN_2 = 7;
 int main() {
     stdio_init_all();
 
+    // Configuração Botão 1
     gpio_init(BTN_PIN);
     gpio_set_dir(BTN_PIN, GPIO_IN);
     gpio_pull_up(BTN_PIN);
 
+    // Configuração Botão 2
     gpio_init(BTN_PIN_2);
     gpio_set_dir(BTN_PIN_2, GPIO_IN);
     gpio_pull_up(BTN_PIN_2);
+
     int cnt_1 = 0;
     int cnt_2 = 0;
 
     while (true) {
-
+        // Lógica para o Botão 1
         if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+            cnt_1++;
+            printf("Botao 1: %d\n", cnt_1);
+            
+            // Debouncing: Espera o botão ser solto
+            while (!gpio_get(BTN_PIN)) {
+                tight_loop_contents();
+            }
+            sleep_ms(50); // Pausa para ignorar o ruído mecânico (bouncing)
         }
 
+        // Lógica para o Botão 2
         if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+            cnt_2++;
+            printf("Botao 2: %d\n", cnt_2);
+            
+            // Debouncing: Espera o botão ser solto
+            while (!gpio_get(BTN_PIN_2)) {
+                tight_loop_contents();
+            }
+            sleep_ms(50); // Pausa para ignorar o ruído mecânico
         }
+
+        tight_loop_contents();
     }
-}
+    }
